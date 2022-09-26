09841aa9edd218292a2ffacd86b9250468e014cc
Ericn01 <eniel662@mtroyal.ca>
2022-09-25 23:12:54 -0600

    First Commit

---
+++ View/home.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'config.inc.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home Page</title>
+   <link href="css/home-styles.css" rel="stylesheet">
+</head>
+<body>
+   <header> </header>
+   <main class="ui grid-container">
+
+   </main>
+   <footer> </footer>
+</body>
+</html>
