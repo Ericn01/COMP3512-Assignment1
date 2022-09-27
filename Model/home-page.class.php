a36c21142077a2329a9898572d38bc9b7a18e349
Ericn01 <eniel662@mtroyal.ca>
2022-09-27 15:44:01 -0600

    *Almost* done the home page queries. Need to test them a little more...

--- Model/home-page.class.php
+++ Model/home-page.class.php
@@ -1,8 +1,13 @@
 <?php
   include "database-connection.class.php";
+  /*
+  * This class will contain the methods that interact with the DB to fulfill the
+  * home page requirements. That is, retrieving the top 10 songs from the given criteria.
+  * A lot of the code here is fairly similar, because the mechanism to retrieve the data is always the same.
+  * The queries themselves change a lot though.
+  */
   class HomeEntries extends DatabaseConnection{
-    private $songLimit = 10;
-
+    # Returns the top genres based on the number of songs within the list.
     function getTopGenres(){
       $sql = "SELECT genres.genre_name AS `Genre`, COUNT(song_id) AS `Song Count`
               FROM genres
@@ -13,31 +18,81 @@
       $results = $this->databaseConnect()->query($sql);
       return $results;
     }
+    # Returns the top artists, based on the number of songs they have in top songs list.
     function getTopArtists(){
-
+      $sql = "SELECT artists.artist_name AS `Artist Name`, COUNT(song_id) AS `Song Count`
+              FROM artists
+              INNER JOIN songs ON artists.artist_id = songs.artist_id
+              GROUP BY artists.artist_id
+              ORDER BY `Song Count` DESC
+              LIMIT 10";
+      $results = $this->databaseConnect()->query($sql);
+      return $results;
     }
+
+    # Returns the most popular songs on spotify within the list.
     function getMostPopularSongs(){
-
+      $sql = "SELECT artists.artist_name AS `Artist Name`, COUNT(song_id) AS `Song Count`
+              FROM artists
+              INNER JOIN songs ON artists.artist_id = songs.artist_id
+              GROUP BY artists.artist_id
+              ORDER BY `Song Count` DESC
+              LIMIT 10";
+      $results = $this->databaseConnect()->query($sql);
+      return $results;
     }
 
+    # Returns artists who only have one song in the DB. Sorted by song popularity.
     function getOneHitWonders(){
-
+      $sql = "SELECT artist_id AS `Artist ID`, popularity AS `Song Popularity`
+              FROM songs
+              WHERE COUNT(artist_id) = 1
+              ORDER BY popularity DESC
+              LIMIT 10";
+      $results = $this->databaseConnect()->query($sql);
+      return $results;
     }
-
+    # Returns the most acoustic songs with the longest length.
     function getLongestAcousticSongs(){
-
+      $sql = "SELECT title AS `Song Title`, acousticness
+              FROM songs
+              WHERE acousticness > 40
+              ORDER BY duration DESC
+              LIMIT 10";
+      $results = $this->databaseConnect()->query($sql);
+      return $results;
     }
-
+    # Best club songs according to danceability -> Calculated field determines Club Suitability score.
     function getClubSongs(){
-
+      $sql = "SELECT title AS `Song Title`, (danceability * 1.6 + energy * 1.4) AS `Clubbing Score`
+              FROM songs
+              WHERE danceability > 80
+              ORDER BY `Clubbing Score` DESC
+              LIMIT 10";
+      $results = $this->databaseConnect()->query($sql);
+      return $results;
     }
-
+    # Query to retrieve the best running songs on Spotify according to the given calculation.
     function getRunningSongs(){
-
+      $sql = "SELECT title AS `Song Title`, (energy * 1.3 + valence * 1.6) AS `Running Score`
+              FROM songs
+              WHERE bpm BETWEEN 120 AND 125
+              ORDER BY `Running Score` DESC
+              LIMIT 10";
+      $results = $this->databaseConnect()->query($sql);
+      return $results;
     }
 
+    # Query to retrieve the best studying songs on Spotify according to the given calculation, and DB.
     function getStudyingSongs(){
-
+      $sql = "SELECT title AS `Song Title`, (acousticness*0.8)+(100-speechiness)+(100-valence) AS `Studying Score`
+              FROM songs
+              WHERE bpm BETWEEN 100 AND 125
+              AND speechiness BETWEEN 1 AND 20
+              ORDER BY `Studying` DESC
+              LIMIT 10";
+      $results = $this->databaseConnect()->query($sql);
+      return $results;
     }
 
   }
--- Model/test.php
+++ Model/test.php
@@ -11,10 +11,18 @@
    <?php
     $test = new HomeEntries();
     $results = $test->getTopGenres();
-    echo "<h1> Top Genres Test </h1>";
-    foreach($results as $row){
 
-      echo "<h4>" . $row['Genre'] . ": " . $row['Song Count'] . " songs</h4>";
+    $artists = $test->getTopArtists();
+    $oneHit = $test->getOneHitWonders();
+    printResults($results, 'Genre', 'Song Count', 'Genres');
+    printResults($artists, 'Artist Name', 'Song Count', 'Artists');
+    printresults($oneHit, 'Artist ID', 'Song Popularity', 'One Hit Wonder');
+
+    function printResults($results, $r1Name, $r2Name, $testName){
+      echo "<h1> Top $testName Test </h1>";
+      foreach($results as $row){
+        echo "<h4>" . $row["$r1Name"] . ": " . $row["$r2Name"] . " songs</h4>";
+      }
     }
    ?>
 </body>
