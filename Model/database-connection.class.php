573db1d8976ccb919938c409c99c81ce8d19085e
Ericn01 <eniel662@mtroyal.ca>
2022-10-04 13:45:18 -0600

    Please work...

--- Model/database-connection.class.php
+++ Model/database-connection.class.php
@@ -1,21 +1,18 @@
 <?php
   class DatabaseConnection{
     # Localhost connection Info
-    
+
     // private $dbHost = 'localhost';
     // private $dbUser = 'root';
     // private $dbPass = '';
     // private $dbName = "music";
 
     // Connecting to the heroku website
-    private $jawsdb_url = parse_url(getenv("JAWSDB_URL"));
-    private $dbHost = $jawsdb_url["host"];
-    private $dbUser = $jawsdb_url["user"];
-    private $dbPass = $jawsdb_url["pass"];
+    private $dbHost = "au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    private $dbUser = "p2fhaaxgj3j8u69t";
+    private $dbPass = "hi54nvd4jqqaio44";
     private $dbName = "bzl6nuctkx4sa2vr";
 
-    $active_group = 'default';
-    $query_builder = TRUE;
     # Establishing a connection with the database
     # We only want classes that extend this class to be able to access the DB, hence protected.
     protected function databaseConnect(){
